0a6265fa20428265e39027c878d077f0fd86127a
Mauricio Correa <frontend.mcu@gmail.com>
2019-03-10 16:33:17 +0100

    media queries, textarea

--- js/main.js
+++ js/main.js
@@ -16,15 +16,18 @@ const app = (function(){
       .getRedirectResult()
         .then( result => {
           if (result.credential) {
+
             console.log('result', result.credential);
             // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
             // You can use these server side with your app's credentials to access the Twitter API.
             const token = result.credential.accessToken;
             const secret = result.credential.secret;
             // ...
+
           }
           // The signed-in user info.
           const user = result.user;
+
         }).catch( error => {
           console.log('error', error);
           // Handle Errors here.
@@ -37,31 +40,53 @@ const app = (function(){
           // ...
         });
 
-    firebase.auth().onAuthStateChanged( user => {
-      if (user) {
-        console.log('user', user);
-        /**
-         * !FIXME - extraer fuera
-         */
-        document.querySelector('#header-user--username').textContent = user.displayName;
-        const img = document.createElement('IMG');
-        img.setAttribute('src', user.photoURL);
-        document.querySelector('.header-avatar').appendChild(img);
-        document.querySelector('.header-user--logout button').addEventListener('click', logOut, false);
-      } else {
-        document.querySelector('#header-welcome').style.display = 'none';
-        const template =
-        `
-        <div class="login-container">
-          <button>login con twitter</button>
-          <p><input type="checkbox" /> ¿Eres admin?</p>
-        </div>
-        `;
+    firebase.auth()
+      .onAuthStateChanged( user => {
+        if (user) {
 
-        main.insertAdjacentHTML('afterbegin', template);
-        document.querySelector('.login-container button').addEventListener('click', loginByTwitter, false);
-      }
-    })
+          console.log('user', user);
+          /**
+           * !FIXME - extraer fuera
+           */
+          document.querySelector('#header-user--username').textContent = user.displayName;
+          const img = document.createElement('IMG');
+          img.setAttribute('src', user.photoURL);
+          document.querySelector('.header-avatar').appendChild(img);
+          document.querySelector('.header-user--logout button')
+            .addEventListener('click', logOut, false);
+
+          const template =
+          `
+            <div class="main-container">
+              <div class="main-container--div">
+                <div class="main-textarea">
+                <textarea id="tweet" name="story" placeholder="tu tweet..."></textarea>
+                </div>
+
+                <button id="send-tweet">Enviar tweet</button>
+              </div>
+            </div>
+          `;
+
+          main.insertAdjacentHTML('afterbegin', template);
+
+        } else {
+
+          document.querySelector('#header-welcome').style.display = 'none';
+          const template =
+          `
+          <div class="main-container">
+            <button>login con twitter</button>
+            <p><input type="checkbox" /> ¿Eres admin?</p>
+          </div>
+          `;
+
+          main.insertAdjacentHTML('afterbegin', template);
+          document.querySelector('.main-container button')
+            .addEventListener('click', loginByTwitter, false);
+
+        }
+      });
   }
 
   function loginByTwitter () {
