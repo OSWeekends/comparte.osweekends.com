5019f8e226625bab3bea7ed6407edc3b670d3639
Mauricio Correa <frontend.mcu@gmail.com>
2019-03-17 16:25:37 +0100

    sendTweet, getTweet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const app = (function(){
 
+  let token;
   const root = null;
   const useHash = true; // Defaults to: false
   const hash = '#!'; // Defaults to: '#'
@@ -10,29 +11,36 @@ const app = (function(){
   const rootDB = firebase.database().ref();
   const users = rootDB.child('users');
 
+  // const tweets = firebase.database().ref(`users/${token}/tweets`);
+
   const user = {
     name: '',
     creationTime: '',
     isAdmin: false,
-    token: ''
-  }
+    id: ''
+  };
 
   router
     .on('/', init)
-    .on('/myprofile', getProfileUser)
+    .on('/admin', getTweets)
     .resolve();
 
   function init() {
     firebase.auth()
       .getRedirectResult()
         .then( result => {
+          console.log('result.credential', result.credential);
+          console.log('result.user', result.user);
+
           if (result.credential) {
-            user.token = result.credential.accessToken;
+            token = result.credential.accessToken;
           }
           if (result.user) {
+            console.log('user.screenName', user.screenName);
+            user.id = result.user.uid;
             user.name = result.user.displayName;
             user.creationTime = result.user.metadata.a;
-            users.child(user.token).set(user);
+            users.child(token).set(user);
           }
         }).catch( error => {
           console.log('error', error);
@@ -49,9 +57,36 @@ const app = (function(){
     firebase.auth()
       .onAuthStateChanged( user => {
         if (user) {
+          console.log('user', user);
+          /**
+           * !FIXME - extraer fuera
+           */
+          document.querySelector('#header-user--username').textContent = user.displayName;
+          const img = document.createElement('IMG');
+          img.setAttribute('src', user.photoURL);
+          document.querySelector('.header-avatar').appendChild(img);
+          document.querySelector('.header-user--logout button')
+            .addEventListener('click', logOut, false);
 
-          router.navigate('/myprofile');
-          getProfileUser (user);
+          const template =
+          `
+            <div class="main-container">
+              <div class="main-container--div">
+                <div class="main-textarea">
+                  <div id="tweet" name="tweet" placeholder="tu tweet..." contentEditable="true"></div>
+                </div>
+
+                <button id="send-tweet">Enviar tweet</button>
+              </div>
+            </div>
+          `;
+
+          main.insertAdjacentHTML('afterbegin', template);
+          document.querySelector('#tweet')
+            .addEventListener('keyup', limitChars, false);
+
+          document.querySelector('#send-tweet')
+            .addEventListener('click', sendTweet, false);
 
         } else {
 
@@ -87,39 +122,6 @@ const app = (function(){
     router.navigate('/');
   }
 
-  function getProfileUser (user) {
-    console.log('user', user);
-
-    if (user) {
-      /**
-       * !FIXME - extraer fuera
-       */
-      document.querySelector('#header-user--username').textContent = user.displayName;
-      const img = document.createElement('IMG');
-      img.setAttribute('src', user.photoURL);
-      document.querySelector('.header-avatar').appendChild(img);
-      document.querySelector('.header-user--logout button')
-        .addEventListener('click', logOut, false);
-
-      const template =
-      `
-        <div class="main-container">
-          <div class="main-container--div">
-            <div class="main-textarea">
-              <div id="tweet" name="tweet" placeholder="tu tweet..." contentEditable="true"></div>
-            </div>
-
-            <button id="send-tweet">Enviar tweet</button>
-          </div>
-        </div>
-      `;
-
-      main.insertAdjacentHTML('afterbegin', template);
-      document.querySelector('#tweet')
-        .addEventListener('keyup', limitChars, false);
-    }
-  }
-
   /**
    * https://codepen.io/gtb104/pen/pztgH
    * @param {*} event
@@ -147,6 +149,76 @@ const app = (function(){
         document.querySelector('#send-tweet').disabled = false;
       }
     }
+  }
+
+  function sendTweet () {
+    const textArea = document.querySelector('#tweet').textContent;
+
+    uid = firebase.auth().currentUser.uid;
+    const user = users.child(token);
+    const tweets = user.child('tweets');
+    const tweet = tweets.push();
+
+    tweet.update({
+      message: textArea,
+      date: new Date().getTime(),
+      published: false,
+      rejected: true,
+    });
+
+  }
+
+  function getTweets () {
+
+    let userToRender = [];
+
+    users.on('value', (snapshot) => {
+
+      snapshot.forEach((users => {
+        let listUser = {};
+
+        listUser.name = users.val().name;
+
+        users.forEach(tweets => {
+          listUser.tweet = [];
+          if (tweets.key === 'tweets') {
+            tweets.forEach(tweet => {
+              listUser.tweet.push(
+                {
+                  id: tweet.key,
+                  message: tweet.val().message,
+                  published: tweet.val().published
+                }
+              );
+            });
+            userToRender.push(listUser);
+            // console.log(userToRender);
+          }
+        });
+      }));
+      renderListTweets(userToRender);
+    });
+
+  }
+
+  function renderListTweets (userToRender) {
+    console.log(userToRender);
+
+    let template;
+
+    userToRender.forEach( group => {
+      template = '<div class="">' +group.name + '</div>';
+      template += '<div>';
+
+      group.tweet.forEach( tweet => {
+        template += '<div class="">' + tweet.message + '</div>';
+        template += '<ul>';
+        template += '<li class=""><button>publicar</button></li>';
+        template += '<li class=""><button>rechazar</button></li>';
+        template += '</ul>';
+      });
+      main.insertAdjacentHTML('afterbegin', template);
+    });
   }
 
 })();
