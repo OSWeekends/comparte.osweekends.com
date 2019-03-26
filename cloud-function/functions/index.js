d07004320ede24487478510fd01d283b7e228441
Mauricio Correa <frontend.mcu@gmail.com>
2019-03-26 23:09:31 +0100

    config.js, twit -S

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// const twit = require('twit');
+// const config = require('./config.js');
+// const Twitter = new twit(config);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send(Twitter);
 });
