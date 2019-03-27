584f7d7bc4054729b8fd91530aa28f58414a9729
Mauricio Correa <frontend.mcu@gmail.com>
2019-03-27 22:38:06 +0100

    updateAndSendTweet

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -1,14 +1,17 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+// admin.initializeApp();
 
-// const twit = require('twit');
-// const config = require('./config.js');
-// const Twitter = new twit(config);
+const twit = require('twit');
+const config = require('./config.js');
+const Twitter = new twit(config);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send(Twitter);
-});
+
+exports.updateAndSendTweet = functions.database.ref('/users/{uid}/tweets/{id}')
+  .onUpdate( (snapshot, context) => {
+    console.log('snapshot', snapshot.before.child('message').val());
+    // console.log('context', context);
+    // console.log('yeap');
+  });
