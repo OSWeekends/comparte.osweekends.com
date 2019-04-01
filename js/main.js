9e164b9be8611ba9b3fd62db138ad48a7768e30a
Mauricio Correa <frontend.mcu@gmail.com>
2019-04-01 22:36:23 +0200

    configuración sw

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+if(navigator.serviceWorker){
+  navigator.serviceWorker.register('./sw.js');
+}
+
 const app = (function(){
 
   const root = null;
---
+++ sw.js
@@ -0,0 +1,73 @@
+const CACHE_STATIC = 'cache-static-v1';
+const CACHE_DINAMIC = 'cache-dinamic-v1';
+const CACHE_INMUTABLE = 'cache-inmuctable-v1';
+
+const APP_SHELL = [
+  '/',
+  'index.html',
+  './css/style.css',
+  '/img/osw-logo.svg',
+  'js/main.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+  'https://fonts.googleapis.com/css?family=Roboto:300,400,700',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
+  'https://www.gstatic.com/firebasejs/5.8.2/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/5.8.2/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/5.8.2/firebase-database.js',
+  '//unpkg.com/navigo@6',
+];
+
+self.addEventListener('install', event => {
+
+  const cacheStatic = caches.open(CACHE_STATIC)
+    .then(cache => {
+      cache.addAll(APP_SHELL);
+    });
+
+  const cacheInmutable = caches.open(CACHE_INMUTABLE)
+    .then(cache => {
+      cache.addAll(APP_SHELL_INMUTABLE);
+    });
+
+  event.waitUntil(
+    Promise.all([cacheStatic, cacheInmutable])
+  );
+
+});
+
+self.addEventListener('activate', event => {
+  const res = caches.keys()
+    .then(keys => {
+      keys.forEach(key => {
+        if (key !== CACHE_STATIC && key.includes('cache-static')) {
+          return caches.delete(key);
+        }
+      });
+    });
+
+  event.waitUntil(res);
+});
+
+self.addEventListener('fetch', event => {
+
+  const respuesta = caches.match(event.request)
+    .then(res => {
+      if (res) {
+        return res;
+      } else {
+        console.log(event.request.url);
+        return fetch(event.request)
+          .then( resRequest => {
+            // caches.open(CACHE_DINAMIC)
+            //   .then(cache => {
+            //     cache.addAll(resRequest.request, res)
+            //   });
+          });
+      }
+
+    });
+
+  event.respondWith(respuesta);
+});
